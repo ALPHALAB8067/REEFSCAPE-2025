0cb0a55a90b8412ff03b12bb4cf10b9770c30645
Stephen-Boiangiu <boiangiu-3200030@eleves.ndl.qc.ca>
2025-03-04 06:39:30 -0800

    Changed txt files to java + removed anything swerve drive related
    **PLEASE ADD GOOD ROBOTCONTAINER**

--- src/main/java/frc/robot/commandgroups/getCoralCMD.java
+++ src/main/java/frc/robot/commandgroups/getCoralCMD.java
@@ -1,13 +1,17 @@
-package frc.robot.commands.commandgroups;
+package frc.robot.commandgroups;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.intake.intakeDownCMD;
+import frc.robot.intake.intakeUpCMD;
+import frc.robot.intake.intakeWheelCMD;
 import frc.robot.subsystems.IntakeSS;
 import frc.robot.subsystems.WristSS;
-import frc.robot.commands.intake.*;
-import frc.robot.commands.wrist.WristGoToAngledCMD;
-import frc.robot.commands.wrist.WristIntakeCMD;
+import frc.robot.wristCommands.WristGoToAngledCMD;
+import frc.robot.wristCommands.WristIntakeCMD;
+import frc.robot.subsystems.IntakeSS;
+import frc.robot.subsystems.WristSS;
 
 //sequential bc we can't make all the commands at once. 
 //we can set parallel command groups in a sequence
